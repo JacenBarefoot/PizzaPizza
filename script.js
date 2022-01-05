d8326f12506406a6fdded80a1ad0e978d298d1d4
Jacen Barefoot <89105120+JacenBarefoot@users.noreply.github.com>
2022-01-05 15:52:36 -0600

    Still working!!

--- script.js
+++ script.js
@@ -1,63 +1,30 @@
-class Pizza {
-    constructor(name, image){
-        this.name = name;
-        this.image = image;
+const itemContainer = document.querySelector(".content")
+const sidesBtn = document.querySelector(".sidesBtn")
+const drinksBtn = document.querySelector(".drinksBtn")
+const pizzaBtn = document.querySelector(".pizzaBtn")
+const dessertBtn = document.querySelector(".dessertBtn")
+
+
+function populateContent(items) {
+    itemContainer.innerHTML = "";
+    for (const item of items) {
+        const price = item.price
+            .split("\n\n")
+            .map((p) => `<p>${p}</p>`)
+            .join("");
+        itemContainer.insertAdjacentHTML(
+            "beforeend",
+            `
+        <article>
+            <h2>${item.name}</h2>
+            ${paragraphs}
+        </article>`
+        )
     }
 }
 
-pizzaList = [new Pizza("Cheese pizza", "images/cheesepizza.jpeg"), new Pizza("Pepperoni Pizza", "images/pepperoni.jpeg"), new Pizza("Sausage Pizza", "images/sausagepizza.jpeg")]
+fetch("food.json").then(req => req.json()).then(items => populateContent())
 
-function pizza_btn() {
-    console.log(pizzaList);
-}
-
-// --------------------------------------------------
-
-class Dessert {
-    constructor(name, image){
-        this.name = name;
-        this.image = image;
-    }
-}
-
-dessertList = [new Dessert("Brownies", "images/brownies.jpeg"), new Dessert("CinnaSticks", "images/cinnasticks.jpeg"), new Dessert("Cookies", "images/cookies.jpeg")]
-
-function dessert_btn() {
-    console.log(dessertList);
-}
-
-// --------------------------------------------------
-
-class Sides {
-    constructor(name, image){
-        this.name = name;
-        this.image = image;
-    }
-}
-
-sidesList = [new Sides("Fries", "images/fries.jpeg"), new Sides("Salad", "images/salad.jpeg"), new Sides("Nachos", "images/nachos.jpeg")]
-
-function sides_btn() {
-    console.log(sidesList);
-}
-
-// --------------------------------------------------
-
-class Drinks {
-    constructor(name, image){
-        this.name = name;
-        this.image = image;
-    }
-}
-
-drinksList = [new Drinks("Water", "images/water.jpeg"), new Drinks("Sprite", "images/sprite.jpeg"), new Drinks("Coke", "images/coke.jpeg")]
-
-function drinks_btn() {
-    console.log(drinksList);
-}
-
-
-// const box = document.querySelector(".content");
-
-
-
+drinksBtn.addEventListener("click", ()=> {
+    populateContent(blah)
+})
