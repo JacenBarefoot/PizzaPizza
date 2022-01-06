dcc2fe847f19cf41966ae3b61ad9b586fe150b70
Dholl205 <dhollingsworth22@basecampcodingacademy.org>
2022-01-05 18:02:12 -0600

    Still working

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-const itemContainer = document.querySelector(".content")
-const sidesBtn = document.querySelector(".sidesBtn")
-const drinksBtn = document.querySelector(".drinksBtn")
-const pizzaBtn = document.querySelector(".pizzaBtn")
-const dessertBtn = document.querySelector(".dessertBtn")
-
+const itemContainer = document.querySelector(".content");
+const sidesBtn = document.querySelector(".sidesBtn");
+const drinksBtn = document.querySelector(".drinksBtn");
+const pizzaBtn = document.querySelector(".pizzaBtn");
+const dessertBtn = document.querySelector(".dessertBtn");
 
 function populateContent(items) {
-    itemContainer.innerHTML = "";
-    for (const item of items) {
-        const price = item.price
-            .split("\n\n")
-            .map((p) => `<p>${p}</p>`)
-            .join("");
-        itemContainer.insertAdjacentHTML(
-            "beforeend",
-            `
+  itemContainer.innerHTML = "";
+  for (const item of items) {
+    const price = item.price
+      .split("\n\n")
+      .map((p) => `<p>${p}</p>`)
+      .join("");
+    itemContainer.insertAdjacentHTML(
+      "beforeend",
+      `
         <article>
             <h2>${item.name}</h2>
             ${paragraphs}
         </article>`
-        )
-    }
+    );
+  }
 }
 
-fetch("food.json").then(req => req.json()).then(items => populateContent())
+fetch("food.json")
+  .then((req) => req.json())
+  .then((items) => populateContent());
 
-drinksBtn.addEventListener("click", ()=> {
-    populateContent(blah)
-})
+drinksBtn.addEventListener("click", () => {
+  populateContent(blah);
+});
